c6841bc3bf8231694c72699729eb6dcb8bdee068
fenghanxu <1162851277@qq.com>
2018-07-06 14:12:27 +0800

    第二次上存

---
+++ Example/FHXColorAndFont/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FHXColorAndFont
+//
+//  Created by fenghanxu on 07/06/2018.
+//  Copyright (c) 2018 fenghanxu. All rights reserved.
+//
+
+import UIKit
+import FHXColorAndFont
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+      view.backgroundColor = Color.blue
+      
+    }
+
+  
+}
+
---
+++ Example/Pods/Target Support Files/Pods-FHXColorAndFont_Tests/Pods-FHXColorAndFont_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FHXColorAndFont_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FHXColorAndFont_Tests
+@end
